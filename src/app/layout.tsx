76530ecc90558b1ae926e8f12a641caf717b2fc5
sfkse <sfkse5591@gmail.com>
2025-07-31 15:54:12 +0200

    adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,6 +39,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8800136508448891"
+          crossOrigin="anonymous"
+        ></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
